1fa693b5b5af7d8d97b4ee1635aed12484f5dea5
parangdajavous <nuit0535@gmail.com>
2025-06-03 15:18:11 +0900

    /users 통합 테스트 (#5)
    /users 통합 테스트 (API에 대한 실패 상황 통합 테스트 코드 동시 작성)

---
+++ src/test/java/co/kr/metacoding/backendtest/integre/UsersControllerTest.java
@@ -0,0 +1,99 @@
+package co.kr.metacoding.backendtest.integre;
+
+import co.kr.metacoding.backendtest.users.Users;
+import co.kr.metacoding.backendtest.users.UsersRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class UsersControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @BeforeEach
+    public void setUp() {
+        // 테스트 시작 전에 실행할 코드
+        System.out.println("setUp");
+        Users 홍길동 = Users.builder()
+                .id(1)
+                .name("홍길동")
+                .build();
+    }
+
+    @AfterEach
+    public void tearDown() { // 끝나고 나서 마무리 함수
+        // 테스트 후 정리할 코드
+        System.out.println("tearDown");
+    }
+
+    @Test
+    public void save_test() throws Exception {
+        // given
+        UsersRequest.SaveDTO reqDTO = new UsersRequest.SaveDTO();
+        reqDTO.setName("임라라");
+
+        String requestBody = om.writeValueAsString(reqDTO);
+        System.out.println(requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/users")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber());
+    }
+
+    @Test
+    public void save_fail_test() throws Exception {
+        // given
+        UsersRequest.SaveDTO reqDTO = new UsersRequest.SaveDTO();
+        reqDTO.setName("홍길동");
+
+        String requestBody = om.writeValueAsString(reqDTO);
+        System.out.println(requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/users")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.reason").exists());
+    }
+
+
+}
